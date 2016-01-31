ba383c88bc67b048386ec68b1830a26715b39931
JP Toto <james.p.toto@gmail.com>
2016-01-30 22:19:01 -0500

    Initial Commit

---
+++ log4net.ES.Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using log4net;
+
+namespace log4net.ES.Example
+{
+    class Program
+    {
+        // Create a new logging instance
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            // Most basic logging example.
+            _log.ErrorFormat("System Error {0}", "Divide by zero error.");
+
+            
+            // Log a message with an exeption object
+            _log.Error("System Error", new Exception("Something terrible happened."));
+
+
+            // Log an exception object with custom fields in the Data property
+            Exception newException = new Exception("There was a system error");
+            newException.Data.Add("CustomProperty", "CustomPropertyValue");
+            newException.Data.Add("SystemUserID", "User43");
+
+            _log.Error("Something broke.", newException);
+
+        }
+    }
+
+ 
+}
---
+++ log4net.ES.Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("log4net.ES.Example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("log4net.ES.Example")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4b79124-e9f0-4944-a3b1-89f1b28e1acb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Add a line to initialize log4net to read the configuration
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+
+// Use the below line if you're using a custom configuration file
+//[assembly: log4net.Config.XmlConfigurator(ConfigFile = "configuration.xml", Watch = true)]`
